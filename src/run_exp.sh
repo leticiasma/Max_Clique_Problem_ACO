97dcb111b85690b7911f135880adfe1bf0972136
leticiasma <leti.ufmg@gmail.com>
2023-07-05 21:24:37 -0300

    Finalizando

--- src/run_exp.sh
+++ src/run_exp.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-DATA_PATH="../data/p_hat700-2.clq"
+DATA_PATH="../data/brock800_4.clq"
 T_MIN=0.1
 T_MAX=6
-N_ANTS=(10 15 20 25 30 35 40)
-N_ITS=100
-EVAP_R=0.1
+N_ANTS=40
+N_ITS=150
+EVAP_R=(0.01 0.05 0.1 0.15 0.2)
 ALPHA=1
 N_RUNS=30
-N_PROCESSES=2
-DATASET_NAME="p_hat700"
+N_PROCESSES=3
+DATASET_NAME="brock800_4"
 RESULTS_TARGET_DIR="../results/$DATASET_NAME"
 
-for i in "${N_ANTS[@]}"
+for i in "${EVAP_R[@]}"
 do
-    echo "Iniciando com N_ANTS = $i"
-	python3 main.py --data_path $DATA_PATH --t_min $T_MIN --t_max $T_MAX --n_ants $i \
-	--n_its $N_ITS --evap_r $EVAP_R --alpha $ALPHA --n_p $N_PROCESSES --n_r $N_RUNS \
+    echo "Iniciando com EVAP_R= $i"
+	python3 main.py --data_path $DATA_PATH --t_min $T_MIN --t_max $T_MAX --n_ants $N_ANTS \
+	--n_its $N_ITS --evap_r $i --alpha $ALPHA --n_p $N_PROCESSES --n_r $N_RUNS \
 	--t_dir $RESULTS_TARGET_DIR
 done
